6993a688dbc661ee7d43aaf59dff67a092c83524
Facundo Lemo <facundolemo@gmail.com>
2024-04-11 21:05:33 -0300

    changes product models and admin

--- miniblog/products/admin.py
+++ miniblog/products/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.utils.html import format_html
 
 from products.models import (
     Category,
@@ -10,16 +11,58 @@ from products.models import (
 class ProductAdmin(admin.ModelAdmin):
     ordering = ("name", "price")  # puede ser tupla o lista.
     search_fields = ("name", "price")
-    # list_filter = ("category",)
-    list_editable = ("price",)
+    list_filter = ("category",)
+    # list_editable = ("price",)
+    readonly_fields = ("name",)
     empty_value_display = "No hay datos para este campo"
 
     list_display = (
         "name",
         "price",
+        "get_stock",
         "description",
         "category",
+        "get_price_range",
+        "get_total",
     )
+    fieldsets = [
+        (
+            "informacion de producto",
+            {
+                "fields": [
+                    "name",
+                    "price",
+                ]
+            },
+        ),
+        (
+            "mas info del producto",
+            {
+                "classes": ["collapse"],
+                "fields": [
+                    "description",
+                    "stock",
+                ],
+            },
+        ),
+    ]
+
+    def get_total(self, obj):  # recibe el obj del admin
+        return obj.price * obj.stock  # ejemplo xd esto iría en venta por ej
+
+    def get_stock(self, obj):
+        POCO = "#FF0000"
+        BIEN = "#00FF00"
+        DEMACIADO = "#FFD300"
+        if obj.stock < 10:
+            codigo = POCO
+        if 10 < obj.stock < 25:
+            codigo = BIEN
+        if obj.stock >= 25:
+            codigo = DEMACIADO
+        return format_html(
+            f'<span style="color:{codigo}">{obj.stock}</span>',
+        )
 
 
 @admin.register(Category)
--- miniblog/products/models.py
+++ miniblog/products/models.py
@@ -1,3 +1,4 @@
+from django.contrib import admin
 from django.db import models
 
 
@@ -12,6 +13,7 @@ class Product(models.Model):
     name = models.CharField(max_length=50)
     description = models.TextField(max_length=200)
     price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.DecimalField(max_digits=6, decimal_places=0)
     category = models.ForeignKey(
         Category,
         on_delete=models.SET_NULL,
@@ -19,5 +21,18 @@ class Product(models.Model):
         blank=True,
     )
 
+    @admin.display(description="Rango de Precios")
+    def get_price_range(self):
+        if self.price >= 55000:
+            return "Alto"
+        if 10000 < self.price < 55000:
+            return "Medio"
+        if self.price <= 10000:
+            return "Bajo"
+
+    # def save(self, *args, **kwargs):
+    #     if self.price > 0:
+    #         super.(self)
+    #     else:
     def __str__(self):
         return self.name
